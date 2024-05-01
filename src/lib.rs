06284b2ffb56541a5820b4c76089fed574e93d49
itmaybehimm <himansupradhan472@gmail.com>
2024-05-01 13:27:47 +0545

    defined lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::error::Error;
+use std::fs;
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.file_path)?;
+    println!("{contents}");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,16 @@
+use minigrep::Config;
 use std::env::{self};
-use std::error::Error;
-use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // dbg!(&args);
-
-    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    // Other way
-    // let contents: String = match fs::read_to_string(file_path) {
-    //     Ok(s) => s,
-    //     Err(e) => panic!("{}", e.to_string()),
-    // };
-    // dbg!(contents);
-
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.file_path)?;
-    println!("{contents}");
-    Ok(())
 }
