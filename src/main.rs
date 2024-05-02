37dd7595e56e09a1647f0690901e2cbf60bcaab8
itmaybehimm <himansupradhan472@gmail.com>
2024-05-02 17:01:29 +0545

    iterator mode

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use minigrep::Config;
 use std::env::{self};
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
